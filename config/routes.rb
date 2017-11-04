4adf399e7b56297a80d3b7c645618eaeb90a1ba3
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2017-11-04 16:09:13 -0700

    show logout button if user is logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :sessions
   resources :users
+
+  root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
