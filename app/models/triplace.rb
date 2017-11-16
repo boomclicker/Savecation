0993afcb9678a5638c74a752a82f93b14934092c
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2017-11-15 21:33:27 -0800

    fix column names

--- app/models/triplace.rb
+++ app/models/triplace.rb
@@ -2,9 +2,8 @@ class Triplace < ApplicationRecord
   belongs_to :city
 
   def self.import(file)
-  	CSV.foreach(file.path, :headers => true) do |row|
-  		Triplace.create! row.to_h
+    CSV.foreach(file.path, :headers => true) do |row|
+      Triplace.create! row.to_h
     end
   end
 end
-
