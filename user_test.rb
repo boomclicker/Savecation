7d9785b3da36433fe0a312e1b66bd027915de715
jjkopo <32941160+jjkopo@users.noreply.github.com>
2017-11-20 19:15:08 -0800

    Delete user_test.rb

--- user_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class UserTest < ActiveSupport::TestCase
- user = User.new
-    assert_not user.valid?
-    assert_equal [:email, :password, :username], user.errors.keys
-  end
-end
