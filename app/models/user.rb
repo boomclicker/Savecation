59777e2af5cff7ae1b0e3c48eb3ada5a4c51d6b5
jjkopo <32941160+jjkopo@users.noreply.github.com>
2017-11-20 19:22:15 -0800

    test fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_secure_password
   validates_uniqueness_of :email
   validates_uniqueness_of :username
+  validates_presence_of :username, :email, :password
 end
