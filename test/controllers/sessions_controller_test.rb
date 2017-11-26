1fafbed4c7a659d93542f01a301b991eb19c4ee7
enhongh <enhongh@cs-sc4080u-a03.cs.surrey.sfu.ca>
2017-11-25 18:44:03 -0800

    sessions test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+ test "should get sessions_new page" do 
+     get create_sessions_url
+    assert_response :success 
+
+ 	
+ end
+
+
 
 end
