0240715bade4b91c864f2817fa146753b3710dc2
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2017-11-27 22:10:26 -0800

    make max a global variable in schedule controller

--- app/controllers/generate_schedule_controller.rb
+++ app/controllers/generate_schedule_controller.rb
@@ -1,32 +1,29 @@
 class GenerateScheduleController < ApplicationController
+
+  $max = 5
+
   def no_budget
-    max = 5
     city_id = params[:city_id]
     days = params[:days]
-
     attractions_list = Triplace.where(city_id: city_id)
 
     if attractions_list.empty?
-      render json: {status: 'FAILURE', message: 'Unable to a schedule', city_id: city_id, data: []}, status: :ok
+      render json: {status: 'FAILURE', message: 'Unable to a schedule - city/attractions not found', city_id: city_id, data: []}, status: :ok
     else
       data = {}
-
       i = 0
       while i < days.to_i
         key = "day"+(i+1).to_s
-        rand_number = rand(1..max)
+        rand_number = rand(1..$max)
         data[key] = rand_number.times.map {attractions_list.sample}
         i+=1
       end
 
       render json: {status: 'SUCCESS', message: 'Created a schedule', city_id: city_id, data: data}, status: :ok
-
     end
-
   end
 
   def budget
-    max = 3
     city_id = params[:city_id]
     days = params[:days].to_i
     budget = params[:budget].to_f
@@ -34,7 +31,7 @@ class GenerateScheduleController < ApplicationController
     attractions_list = Triplace.where(city_id: city_id)
 
     if attractions_list.empty?
-      render json: {status: 'FAILURE', message: 'Unable to a schedule', city_id: city_id, data: {}}, status: :ok
+      render json: {status: 'FAILURE', message: 'Unable to a schedule - city/attractions not found', city_id: city_id, data: []}, status: :ok
     else
 
       data = {}
@@ -42,7 +39,7 @@ class GenerateScheduleController < ApplicationController
       total = 0
       while i < days
         key = 'day'+(i+1).to_s
-        rand_number = rand(1..max)
+        rand_number = rand(1..$max)
         day_list = rand_number.times.map {attractions_list.sample}
         sum_day = day_list.inject(0) {|sum, day_list_inner| sum + day_list_inner[:price]}
         if sum_day >= (0.99) * (budget_per_day) and sum_day <= (budget_per_day)
@@ -53,11 +50,7 @@ class GenerateScheduleController < ApplicationController
       end
 
       puts 'Total = $' + total.to_s
-
       render json: {status: 'SUCCESS', message: 'Created a schedule', city_id: city_id, data: data}, status: :ok
-
     end
-
   end
-
 end
