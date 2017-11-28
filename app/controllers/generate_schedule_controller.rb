cd0cfcc5ad6bfc562e56325b526b3e3d857ab58f
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2017-11-28 02:51:09 -0800

    return price list for each day and total price for schedule in API calls

--- app/controllers/generate_schedule_controller.rb
+++ app/controllers/generate_schedule_controller.rb
@@ -4,21 +4,38 @@ class GenerateScheduleController < ApplicationController
 
   def no_budget
     city_id = params[:city_id]
-    days = params[:days]
+    days = params[:days].to_i
     attractions_list = Triplace.where(city_id: city_id)
 
     if attractions_list.empty?
       render json: {status: 'FAILURE', message: 'Unable to a schedule - city/attractions not found', city_id: city_id, data: []}, status: :ok
     else
       data = {}
+      visited_ids = []
+      price_list = []
       i = 0
-      while i < days.to_i
-        key = "day"+(i+1).to_s
+      total = 0
+      while i < days
+        key = 'day'+(i+1).to_s
         rand_number = rand(1..$max)
-        data[key] = rand_number.times.map {attractions_list.sample}
-        i+=1
+        day_list = rand_number.times.map {attractions_list.sample}
+        ids_in_day_list = day_list.map {|x| x[:id]}
+        if (visited_ids & ids_in_day_list).length > 0
+          next
+        else
+          sum_day = day_list.inject(0) {|sum, day_list_inner| sum + day_list_inner[:price]}
+          puts 'Day ' + (i+1).to_s + ' cost = ' + sum_day.to_s
+          price_list.push(sum_day)
+          total += sum_day
+          data[key] = day_list
+          visited_ids += ids_in_day_list
+          i += 1
+        end
       end
-      render json: {status: 'SUCCESS', message: 'Created a schedule', city_id: city_id, data: data}, status: :ok
+
+      puts 'Total = $' + total.to_s
+      render json: {status: 'SUCCESS', message: 'Created a schedule', city_id: city_id, data: data,
+                    price_list: price_list, total: total}, status: :ok
     end
   end
 
@@ -33,9 +50,10 @@ class GenerateScheduleController < ApplicationController
       render json: {status: 'FAILURE', message: 'Unable to a schedule - city/attractions not found', city_id: city_id, data: []}, status: :ok
     else
       data = {}
+      visited_ids = []
+      price_list = []
       i = 0
       total = 0
-      visited_ids = []
       count_loop = 0
       total_loops = 0
       while i < days
@@ -49,12 +67,13 @@ class GenerateScheduleController < ApplicationController
         else
           sum_day = day_list.inject(0) {|sum, day_list_inner| sum + day_list_inner[:price]}
           if sum_day >= (0.99 * budget_per_day) and sum_day <= budget_per_day
-            puts 'adding stuff to day ' + (i+1).to_s + '. Loop ran ' + count_loop.to_s + ' times.'
+            puts 'Day ' + (i+1).to_s + ' cost = ' + sum_day.to_s + '. Loop ran ' + count_loop.to_s + ' times.'
             total_loops += count_loop
             count_loop = 0
             data[key] = day_list
             i += 1
             total += sum_day
+            price_list.push(sum_day)
             visited_ids += ids_in_day_list
           end
         end
@@ -62,7 +81,8 @@ class GenerateScheduleController < ApplicationController
 
       puts 'Total = $' + total.to_s
       puts 'Total loops = ' + total_loops.to_s
-      render json: {status: 'SUCCESS', message: 'Created a schedule', city_id: city_id, data: data}, status: :ok
+      render json: {status: 'SUCCESS', message: 'Created a schedule', city_id: city_id, data: data,
+                    price_list: price_list, total: total}, status: :ok
     end
   end
 end
