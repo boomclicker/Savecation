a1452a99a4b78fb235cfe0f473827c56eb7d5722
jjkopo <32941160+jjkopo@users.noreply.github.com>
2017-12-02 21:13:50 -0800

    Update city_test.rb

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "valid city" do
+    newcity = City.new
+    assert_not newcity.valid?
+  end
+
+
+  test "city must be unique" do
+    newcity = City.new
+    duplicate_city = newcity.dup
+    newcity.save
+    assert_not duplicate_city.valid?
+  end
 end
