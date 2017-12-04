540b569a3679136cc6d413ac52daec164f3d4aac
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2017-12-04 01:53:13 -0800

    show days, budget on schedule page

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,10 +5,10 @@ class SchedulesController < ApplicationController
   def index
     city_id = params[:city_id]
     @city_name = City.find(id=city_id).name
-    days = params[:days]
-    budget = params[:budget]
+    @days = params[:days]
+    @budget = params[:budget]
 
-    @url = 'http://pure-coast-27115.herokuapp.com/generate_schedule/city/' + city_id + '/days/' + days + '/budget/' + budget
+    @url = 'http://pure-coast-27115.herokuapp.com/generate_schedule/city/' + city_id + '/days/' + @days + '/budget/' + @budget
     @attraction_base_url = 'http://pure-coast-27115.herokuapp.com/attraction_detail/'
 
     @result = JSON.load(open(@url).read)
