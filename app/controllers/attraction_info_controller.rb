07b9cf7ee431174aa161af32600284a13f4c08af
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2017-12-04 13:58:16 -0800

    mapspls

--- app/controllers/attraction_info_controller.rb
+++ app/controllers/attraction_info_controller.rb
@@ -5,6 +5,7 @@ class AttractionInfoController < ApplicationController
     @title = attraction.title
     @city_name = City.find(id=attraction.city_id).name
     @price = attraction.price
-    @src = "//www.google.com/maps/embed/v1/place?q=" + @title + "&zoom=17&key=AIzaSyBdHe-dzBXY0NyfgJotSAV_x08ElhZIk7c"
+    @src = "//www.google.com/maps/embed/v1/place?q=" + @title + "&zoom=17&key=AIzaSyBdHe-dzBXY0NyfgJotSAV_x08ElhZIk7c".to_s
+    puts @src
   end
 end
