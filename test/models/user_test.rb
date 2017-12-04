604259d00f574e98656cb093885b0fc0224192a2
jjkopo <32941160+jjkopo@users.noreply.github.com>
2017-12-04 15:32:16 -0800

    Update user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,4 +18,25 @@ test "Requires all fields" do
     assert_not duplicate_user.valid?
   end
 
+test "must have email" do
+  user = User.new
+  assert_not_nil user.email, "email must be entered"
+end
+
+test "must have username" do
+  user = User.new
+  assert_not_nil user.username, "username must be entered"
+end
+
+test "must have password" do
+  user = User.new
+  assert_not_nil user.password, "password must be entered"
+end
+
+test "password must match" do
+  user = User.new
+  assert_equal user.password, user.password
+end
+
+
 end
