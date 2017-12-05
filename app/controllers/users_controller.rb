da37522c42615810210ff52c43cd9fe8303d89ab
enhongh <enhongh@asb9838nu-c12.csil.sfu.ca>
2017-12-04 16:34:30 -0800

    new admin destroy page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def index
+    @user = User.find(current_user.id)
+    if @user.isAdmin
+      @users = User.all
+    end
+  end
 
   def create
     @user = User.new(user_params)
@@ -21,7 +27,14 @@ class UsersController < ApplicationController
       @users = User.all
     end
   end
-
+ def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
   private
 
   def user_params
